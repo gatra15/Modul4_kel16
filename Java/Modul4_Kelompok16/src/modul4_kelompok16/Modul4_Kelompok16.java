e908e57c8d57a04ecc7efae3e580b2b5fc241db0
Galih Saputra <galih881@gmail.com>
2020-04-04 11:11:14 +0700

    Praktikum modul 4

---
+++ Java/Modul4_Kelompok16/src/modul4_kelompok16/Modul4_Kelompok16.java
@@ -0,0 +1,13 @@
+package modul4_kelompok16;
+
+
+public class Modul4_Kelompok16 {
+
+    static void cetak(String kelompok){
+        System.out.println(kelompok);
+    }
+    public static void main(String[] args) {
+        cetak("Kelompok 16");
+    }
+    
+}
